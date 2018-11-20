dc74c9955eeb787e9fc0b7ba681c1ae4c954956e
zaboltar <cletothar@gmail.com>
2018-11-20 06:52:41 -0500

    moreLvlDetails

---
+++ CretasicBlood UnityProject/Assets/playerTrascend.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerTrascend : MonoBehaviour {
+
+	static public playerTrascend pT;
+
+	// Use this for initialization
+	void Awake() {
+		 if (pT == null) {
+
+            pT = this;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject);    
+        }
+	}
+	
+	
+}
