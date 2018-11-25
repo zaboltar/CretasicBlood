3198f0ba038acb539454e0555e7b08019f807e67
zaboltar <cletothar@gmail.com>
2018-11-24 21:38:47 -0500

    cinematicsAvailable

---
+++ CretasicBlood UnityProject/Assets/cinemaPolitics.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cinemaPolitics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(JumpToScene());
+	}
+
+	IEnumerator JumpToScene() {
+		yield return new WaitForSeconds(8f);
+		SceneManager.LoadScene(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
