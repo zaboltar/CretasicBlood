f630b7e825c2afd8fd731b5209c77486e2a28437
zaboltar <cletothar@gmail.com>
2018-11-26 15:48:39 -0500

    MuseFromHell added

---
+++ CretasicBlood UnityProject/Assets/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+static public MusicManager audioMngr;
+    // la instancia se crea y se guarda en esta variable
+	void Awake() {
+         // se refiere a la instancia creada de audiomanager
+
+       /* if (audioMngr == null) {
+
+            audioMngr = this;
+            DontDestroyOnLoad(transform.gameObject);
+        } else {
+            Destroy(gameObject);    
+        }*/
+	}
+}
